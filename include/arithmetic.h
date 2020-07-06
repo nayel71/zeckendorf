722a6aed7fbc3f294eeb7774c5bef2c2b569d3a0
Samin Riasat <nayel71@gmail.com>
2020-07-06 11:55:31 -0400

    corrected arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -2,7 +2,7 @@
 #define ARITHMETIC_H
 
 #include <stdbool.h>
-#include "zeckendorf.h" // zint, zrep, zint_is_valid, zrep_is_valid
+#include "zeckendorf.h" // zrep, zrep_is_valid
 
 // z_add(z1, z2) returns the sum of z1 and z2
 // requires: zrep_is_valid(z1) && zrep_is_valid(z2)
