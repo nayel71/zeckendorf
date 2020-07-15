bfda98e26d6f0d1097d5507dcda7fe0b0dc68402
Samin Riasat <nayel71@gmail.com>
2020-07-14 22:41:36 -0400

    add.h: corrected documentation

--- src/add.h
+++ src/add.h
@@ -3,5 +3,5 @@
 // add_len(s1, s2, len1, len2, len) returns the string representation of the sum of the
 // Zeckendorf representations given by s1 and s2, and stores its length at rlen if rlen != NULL
 // requires: strlen(s1) == len1 && strlen(s2) == len2
-// effects: allocates memory (caller must free), updates *rlen if len != NULL
+// effects: allocates memory (caller must free), updates *rlen if rlen != NULL
 char *add_len(const char *s1, const char *s2, const int len1, const int len2, int *rlen);
